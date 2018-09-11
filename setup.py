2cc3b2e5e839b12f284a012ca7c21c9a3269013e
Tooooommy <=>
2018-09-11 16:01:39 +0800

    修改：     setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="requests_spider",
-    version="0.0.7",
+    version="0.0.8",
     description="Web crawling framework like flask.",
     author="Tommy",
     long_description=open('README.md').read(),
